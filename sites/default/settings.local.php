71ee96bab08d3985d3075c3a0fe294d4eeaae836
Alistair Martin <info@alimartin.com>
2023-02-16 08:07:48 +0000

    Local settings file

---
+++ sites/default/settings.local.php
@@ -0,0 +1,14 @@
+<?php
+
+$settings['update_free_access'] = FALSE;
+$settings['file_private_path'] = '/private-files';
+$settings['file_temp_path'] = '/tmp';
+
+$settings['trusted_host_patterns'] = [
+  '^hyperluminal\.co\.uk$',
+  '^.+\.hyperluminal\.co\.uk$',
+  '^hyperluminal\.ddev\.site$',
+  '^.+\.hyperluminal\.ddev\.site$',
+];
+
+$settings['config_sync_directory'] = '/sync';
